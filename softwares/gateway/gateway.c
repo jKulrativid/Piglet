ddb8a9f395da32456820dd8bf063185106cde8ef
K. Jetnipit <jetnipit.kul@gmail.com>
2024-04-21 16:24:22 +0700

    add scaffolding on gateway

--- softwares/gateway/gateway.c
+++ softwares/gateway/gateway.c
@@ -1,67 +1,115 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <pcap/pcap.h>  /* GIMME a libpcap plz! */
-#include <errno.h>
-#include <sys/socket.h>
+#include <pcap/pcap.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <net/ethernet.h>
 
-int main(int argc, char **argv)
+void my_packet_handler(
+    u_char *args,
+    const struct pcap_pkthdr *header,
+    const u_char *packet
+)
 {
-  char *dev; /* name of the device to use */ 
-  char *net; /* dot notation of the network address */
-  char *mask;/* dot notation of the network mask    */
-  int ret;   /* return code */
-  char errbuf[PCAP_ERRBUF_SIZE];
-  bpf_u_int32 netp; /* ip          */
-  bpf_u_int32 maskp;/* subnet mask */
-  struct in_addr addr;
+    /* First, lets make sure we have an IP packet */
+    struct ether_header *eth_header;
+    eth_header = (struct ether_header *) packet;
+    if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
+        printf("Not an IP packet. Skipping...\n\n");
+        return;
+    }
 
-  /* ask pcap to find a valid device for use to sniff on */
-  dev = pcap_lookupdev(errbuf);
+    /* The total packet length, including all headers
+       and the data payload is stored in
+       header->len and header->caplen. Caplen is
+       the amount actually available, and len is the
+       total packet length even if it is larger
+       than what we currently have captured. If the snapshot
+       length set with pcap_open_live() is too small, you may
+       not have the whole packet. */
+    printf("Total packet available: %d bytes\n", header->caplen);
+    printf("Expected packet size: %d bytes\n", header->len);
 
-  /* error checking */
-  if(dev == NULL)
-  {
-   printf("%s\n",errbuf);
-   exit(1);
-  }
+    /* Pointers to start point of various headers */
+    const u_char *ip_header;
+    const u_char *tcp_header;
+    const u_char *payload;
 
-  /* print out device name */
-  printf("DEV: %s\n",dev);
+    /* Header lengths in bytes */
+    int ethernet_header_length = 14; /* Doesn't change */
+    int ip_header_length;
+    int tcp_header_length;
+    int payload_length;
 
-  /* ask pcap for the network address and mask of the device */
-  ret = pcap_lookupnet(dev,&netp,&maskp,errbuf);
+    /* Find start of IP header */
+    ip_header = packet + ethernet_header_length;
+    /* The second-half of the first byte in ip_header
+       contains the IP header length (IHL). */
+    ip_header_length = ((*ip_header) & 0x0F);
+    /* The IHL is number of 32-bit segments. Multiply
+       by four to get a byte count for pointer arithmetic */
+    ip_header_length = ip_header_length * 4;
+    printf("IP header length (IHL) in bytes: %d\n", ip_header_length);
 
-  if(ret == -1)
-  {
-   printf("%s\n",errbuf);
-   exit(1);
-  }
+    /* Now that we know where the IP header is, we can 
+       inspect the IP header for a protocol number to 
+       make sure it is TCP before going any further. 
+       Protocol is always the 10th byte of the IP header */
+    u_char protocol = *(ip_header + 9);
+    if (protocol != IPPROTO_TCP) {
+        printf("Not a TCP packet. Skipping...\n\n");
+        return;
+    }
 
-  /* get the network address in a human readable form */
-  addr.s_addr = netp;
-  net = inet_ntoa(addr);
+    /* Add the ethernet and ip header length to the start of the packet
+       to find the beginning of the TCP header */
+    tcp_header = packet + ethernet_header_length + ip_header_length;
+    /* TCP header length is stored in the first half 
+       of the 12th byte in the TCP header. Because we only want
+       the value of the top half of the byte, we have to shift it
+       down to the bottom half otherwise it is using the most 
+       significant bits instead of the least significant bits */
+    tcp_header_length = ((*(tcp_header + 12)) & 0xF0) >> 4;
+    /* The TCP header length stored in those 4 bits represents
+       how many 32-bit words there are in the header, just like
+       the IP header length. We multiply by four again to get a
+       byte count. */
+    tcp_header_length = tcp_header_length * 4;
+    printf("TCP header length in bytes: %d\n", tcp_header_length);
 
-  if(net == NULL)/* thanks Scott :-P */
-  {
-    perror("inet_ntoa");
-    exit(1);
-  }
+    /* Add up all the header sizes to find the payload offset */
+    int total_headers_size = ethernet_header_length+ip_header_length+tcp_header_length;
+    printf("Size of all headers combined: %d bytes\n", total_headers_size);
+    payload_length = header->caplen -
+        (ethernet_header_length + ip_header_length + tcp_header_length);
+    printf("Payload size: %d bytes\n", payload_length);
+    payload = packet + total_headers_size;
+    printf("Memory address where payload begins: %p\n\n", payload);
 
-  printf("NET: %s\n",net);
+    /* Print payload in ASCII */
+    /*  
+    if (payload_length > 0) {
+        const u_char *temp_pointer = payload;
+        int byte_count = 0;
+        while (byte_count++ < payload_length) {
+            printf("%c", *temp_pointer);
+            temp_pointer++;
+        }
+        printf("\n");
+    }
+    */
 
-  /* do the same as above for the device's mask */
-  addr.s_addr = maskp;
-  mask = inet_ntoa(addr);
-  
-  if(mask == NULL)
-  {
-    perror("inet_ntoa");
-    exit(1);
-  }
-  
-  printf("MASK: %s\n",mask);
+    return;
+}
 
-  return 0;
+int main(int argc, char **argv) {
+    pcap_t *handle;
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    char *device = "eth1";
+    int snapshot_len = 1028;
+    int promiscuous = 0;
+    int timeout = 1000;
+
+    handle = pcap_open_live(device, snapshot_len, promiscuous, timeout, error_buffer);
+    pcap_loop(handle, -1, my_packet_handler, NULL);
+    pcap_close(handle);
+    return 0;
 }
