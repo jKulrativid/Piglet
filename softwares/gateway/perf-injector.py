fcc1b057b81bcdb7898601b2bf036028cf00f3e2
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2024-05-09 19:07:46 +0700

    fix perf-injector

--- softwares/gateway/perf-injector.py
+++ softwares/gateway/perf-injector.py
@@ -1,8 +1,8 @@
+from multiprocessing import Process
+import os
 from scapy.all import Ether, IP, UDP
 from scapy.all import *
 
-# for multiprocessing
-from multiprocessing import Process
 
 # try to send a packet to the target via an interface dev
 def send_packet(dev, packet):
@@ -10,13 +10,10 @@ def send_packet(dev, packet):
     print(f"Sent packet via {dev}")
 
 # try to send a packet to the target via an interface dev
-packet1 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"0"*1000)
-packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"1"*200)
 # 53 - len("Hello World") = 42
 # packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.199", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World")
 # for i in range(100):
 #     send_packet("piglet-loopback", packet1)
-# send_packet("piglet-vin", packet2)
 
 # faster sending
 
@@ -24,14 +21,29 @@ packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.22
 
 # sendpfast with packet1 and 2 simultaneously with different thread
 
-p1 = Process(target=sendpfast, args=(packet1, ), kwargs={"iface": "piglet-loopback", "loop": 200, "file_cache": True, "mbps": 50})
 
-p2 = Process(target=sendpfast, args=(packet2, ), kwargs={"iface": "piglet-loopback", "loop": 400, "file_cache": True, "mbps": 50})
-
-p1.start()
-p2.start()
-
-p1.join()
-p2.join()
 
 print("Done")
+
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+if __name__ == '__main__':
+    info('main line')
+
+    packet1 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"0"*1000)
+    packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"1"*200)
+
+    p1 = Process(target=sendpfast, args=(packet1, ), kwargs={"iface": "en13", "loop": 20000, "file_cache": True, "mbps": 30})
+
+    p2 = Process(target=sendpfast, args=(packet2, ), kwargs={"iface": "en13", "loop": 40000, "file_cache": True, "mbps": 30})
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
