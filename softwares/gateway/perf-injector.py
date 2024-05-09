f656e8f9bbf8fc82e6ee986a3034bb014247f60a
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2024-05-09 19:12:07 +0700

    wip

--- softwares/gateway/perf-injector.py
+++ softwares/gateway/perf-injector.py
@@ -35,15 +35,23 @@ def info(title):
 if __name__ == '__main__':
     info('main line')
 
-    packet1 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"0"*1000)
-    packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.111.222.123")/UDP(dport=1234)/Raw(load=b"Hello World"+ b"1"*200)
+    packet1 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.168.1.56")/TCP(dport=1234)/Raw(load=b"Hello World"+ b"0"*0)
+    packet2 = Ether(dst="08:00:27:00:00:01")/IP(src="10.147.18.200", dst="192.168.1.56")/TCP(dport=1234)/Raw(load=b"Hello World"+ b"1"*0)
 
-    p1 = Process(target=sendpfast, args=(packet1, ), kwargs={"iface": "en13", "loop": 20000, "file_cache": True, "mbps": 30})
+    p1 = Process(target=sendpfast, args=(packet1, ), kwargs={"iface": "en13", "loop": 1, "file_cache": True, "mbps": 30})
 
-    p2 = Process(target=sendpfast, args=(packet2, ), kwargs={"iface": "en13", "loop": 40000, "file_cache": True, "mbps": 30})
+    p2 = Process(target=sendpfast, args=(packet2, ), kwargs={"iface": "en13", "loop": 1, "file_cache": True, "mbps": 30})
 
     p1.start()
     p2.start()
 
     p1.join()
     p2.join()
+
+
+'''
+is_safe = is_safe && !(((hdr.ipv4.src & 0x0) == 0x00000000) && ((hdr.ipv4.dst & 0xffffff00) == 0xc0a80138) && ((hdr.tcp.dst_port == 22)));
+is_safe = is_safe && !(((hdr.ipv4.src & 0x0) == 0x00000000) && ((hdr.ipv4.dst & 0xffffff00) == 0xc0a80138) && ((hdr.tcp.dst_port == 1433)));
+is_safe = is_safe && !(((hdr.ipv4.src & 0x0) == 0x00000000) && ((hdr.ipv4.dst & 0xffffff00) == 0xc0a80138) && ((hdr.tcp.dst_port == 80)));
+is_safe = is_safe && !(((hdr.ipv4.src & 0x0) == 0x00000000) && ((hdr.ipv4.dst & 0xffffff00) == 0xc0a80138) && ((hdr.tcp.dst_port == 21)));
+'''
