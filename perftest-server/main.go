2e860dfac1671489e4fa2ff81e96ed5ae1e900af
Jetnipit Kulrativid <jetnipit54@gmail.com>
2024-05-16 19:20:29 +0700

    add perftest server

--- perftest-server/main.go
+++ perftest-server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/fatih/structs"
 	"github.com/gin-gonic/gin"
@@ -19,22 +23,26 @@ const (
 )
 
 type PerfResult struct {
-	Pps      float64
-	Mbps     float64
-	Duration float64
-	Count    int
-	Status   Status
-	Err      error
+	Pps        float64
+	Mbps       float64
+	DurationNs int
+	Count      int
+	Raw        string
+	Status     Status
+	Err        error
 }
 
 var database map[string]*PerfResult
 
-func runPerf(id string, ethIface string, pktlen int, pktcnt int) {
+func runPerf(id string, ethIface string, pktlen, pktcnt, timeout string) {
 	database[id] = &PerfResult{Status: PerfRunning}
-
-	cmdText := fmt.Sprintf("./perf-sniffer %s \"\" %d %d 0 0", ethIface, pktlen, pktcnt)
-	out, err := exec.Command(cmdText).Output()
+	cmd := `-c "./perf-sniffer enx2887ba3e44aa \"\" 70 100 0 0 10"`
+	c := exec.Command("bash", strings.Fields(cmd)...)
+	out, err := c.CombinedOutput()
 	if err != nil {
+		fmt.Println(string(out[:]))
+		fmt.Println(c.Path, c.Args)
+		log.Fatal(err)
 		database[id] = &PerfResult{
 			Status: PerfErr,
 			Err:    err,
@@ -42,14 +50,60 @@ func runPerf(id string, ethIface string, pktlen int, pktcnt int) {
 		return
 	}
 
-	database[id] = &PerfResult{
-		Pps:      1.2,
-		Mbps:     3.4,
-		Duration: 4.5,
-		Count:    888,
-		Status:   PerfFinished,
-		Err:      nil,
+	outraw := string(out[:])
+
+	result := &PerfResult{}
+
+	lines := strings.Split(strings.ReplaceAll(outraw, "\r\n", "\n"), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "Mbps") {
+			re := regexp.MustCompile(`^Throughput:\s*(\d+)\s*Mbps$`)
+			match := re.FindStringSubmatch(line)
+			mbps, err := strconv.Atoi(match[0])
+			if err != nil {
+				result.Status = PerfErr
+				result.Err = err
+				return
+			}
+			result.Mbps = float64(mbps)
+		} else if strings.Contains(line, "pps") {
+			re := regexp.MustCompile(`^Packet per second:\s*(\d+)\s*pps$`)
+			match := re.FindStringSubmatch(line)
+			pps, err := strconv.Atoi(match[0])
+			if err != nil {
+				result.Status = PerfErr
+				result.Err = err
+				return
+			}
+			result.Pps = float64(pps)
+		} else if strings.Contains(line, "pkt1 count") {
+			re := regexp.MustCompile(`^overall pkt1 count = \s*(\d+)\s*$s`)
+			match := re.FindStringSubmatch(line)
+			cnt, err := strconv.Atoi(match[0])
+			if err != nil {
+				result.Status = PerfErr
+				result.Err = err
+				return
+			}
+			result.Count = cnt
+		} else if strings.Contains(line, "Duration") {
+			re := regexp.MustCompile(`^Duration:\s*\S+\s*s,\s*(\d+)\s*ns$`)
+			match := re.FindStringSubmatch(line)
+			ns, err := strconv.Atoi(match[0])
+			if err != nil {
+				result.Status = PerfErr
+				result.Err = err
+				return
+			}
+			result.DurationNs = ns
+		}
 	}
+
+	result.Raw = outraw
+	result.Status = PerfFinished
+	result.Err = nil
+
+	database[id] = result
 }
 
 func runPerfAsync(c *gin.Context) {
@@ -58,9 +112,42 @@ func runPerfAsync(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err,
 		})
+		return
 	}
 
-	go runPerf(uuid.String(), "eth0", 1, 1)
+	ethInterface, ok := c.GetQuery("eth")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "query param 'eth' required",
+		})
+		return
+	}
+
+	pktLen, ok := c.GetQuery("pktlen")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "query param 'pktlen' required",
+		})
+		return
+	}
+
+	pktCnt, ok := c.GetQuery("pktcnt")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "query param 'pktcnt' required",
+		})
+		return
+	}
+
+	timeout, ok := c.GetQuery("timeout")
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "query param 'timeout' required",
+		})
+		return
+	}
+
+	go runPerf(uuid.String(), ethInterface, pktLen, pktCnt, timeout)
 
 	c.JSON(http.StatusOK, gin.H{
 		"id": uuid.String(),
